32528254f1562fe39492a4c45b63fa1257b80a25
rgreenan55 <rjgreenan55@gmail.com>
2024-05-26 14:11:09 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Grid, Paper } from "@mui/material"
+
+function App() {
+  return (
+    <Grid>
+        <Paper>
+            Hello
+        </Paper>
+    </Grid>
+  )
+}
+
+export default App
