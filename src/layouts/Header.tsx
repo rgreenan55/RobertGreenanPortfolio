108a7d4b07ad24ec56a85dadd137e63947b61199
rgreenan55 <rjgreenan55@gmail.com>
2024-06-08 19:03:38 -0300

    Added Fun Button

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-import { AppBar, Avatar, Box, Container, IconButton, Toolbar, Typography, useTheme } from "@mui/material";
-import { GitHub, LightMode, LinkedIn, NightsStay as DarkMode } from "@mui/icons-material";
+import { AnimatePresence, motion } from 'framer-motion';
+import { AppBar, Avatar, Box, Container, IconButton, Toolbar, Tooltip, Typography, useTheme } from "@mui/material";
+import { GitHub, LightMode, LinkedIn, NightsStay as DarkMode, SentimentVerySatisfied, SentimentSatisfied, SentimentNeutral, SentimentDissatisfied, SentimentVeryDissatisfied } from "@mui/icons-material";
 import logo from '../assets/images/logo.png';
 import { ThemeContext } from '../Themes';
+
+const moodIcons = [
+    { text: '', icon: <SentimentVerySatisfied /> },
+    { text: 'Hey there!', icon: <SentimentVerySatisfied /> },
+    { text: 'Hey, again!', icon: <SentimentVerySatisfied /> },
+    { text: 'Ouch', icon: <SentimentSatisfied /> },
+    { text: 'That kinda hurts you know', icon: <SentimentNeutral /> },
+    { text: 'Knock it off you would?', icon: <SentimentDissatisfied /> },
+    { text: 'Seriously, stop it', icon: <SentimentDissatisfied /> },
+    { text: '...', icon: <SentimentVeryDissatisfied /> }
+]
 
 const Header = () => {
     const theme = useTheme();
     const setThemeName = React.useContext(ThemeContext);
     const [animation, setAnimation] = React.useState(false);
     const [darkMode, setDarkMode] = React.useState(true);
-    
+    const [moodIndex, setMoodIndex] = React.useState(0);
+    const [showMood, setShowMood] = React.useState(false);
 
     // Handles Light/Dark Mode Switching
     const handleLightDarkModeToggle = () => {
@@ -19,8 +31,20 @@ const Header = () => {
         setTimeout(() => {
             setDarkMode(!darkMode);
             setThemeName(darkMode ? 'lightTheme' : 'darkTheme');
+            if (moodIndex >= moodIcons.length) {
+                setMoodIndex(moodIcons.length-1);
+            }
         }, 500);
     };
+
+    const handleMoodTooltip = () => {
+        setMoodIndex(moodIndex+1);
+        setShowMood(true);
+
+        setTimeout(() => {
+            setShowMood(false);
+        }, 1000);
+    }
 
     return (
         <Box flexGrow={1} sx={{ mb: '80px' }}>
@@ -36,6 +60,35 @@ const Header = () => {
 
                     {/* Links - Icon Button */}
                     <Box display='flex'>
+                        
+                        {/* Playful Mood Button */}
+                        <AnimatePresence>
+                            {moodIndex < moodIcons.length &&
+                                <Box sx={{ backgroundColor: darkMode ? 'white' : '#171717' }}>
+                                    <Box
+                                        component={motion.div}
+                                        transition={{ duration: 0.5 }}
+                                        exit={{ rotateY: 180 }}
+                                        sx={{ backgroundColor: theme.palette.primary.main }}
+                                    >
+                                        <IconButton
+                                            component={motion.div}
+                                            transition={{ duration: 0, delay: 0.15 }}
+                                            exit={{ opacity: 0 }}
+                                            disabled={showMood}
+                                            onClick={handleMoodTooltip}
+                                            sx={{ ':first-child': { color: darkMode ? 'white' : '#171717' }}}
+                                        >
+                                            <Tooltip open={showMood} title={moodIcons[moodIndex].text} arrow>
+                                                {moodIcons[moodIndex].icon}
+                                            </Tooltip>
+                                        </IconButton>
+                                    </Box>
+                                </Box>
+                            }
+                        </AnimatePresence>
+
+                        {/* Links */}
                         <IconButton onClick={() => window.open('https://github.com/rgreenan55', '_blank')} sx={{ borderRadius: '0' }}>
                             <GitHub sx={{ color: darkMode ? 'white' : '#171717' }} />
                         </IconButton>
@@ -44,7 +97,8 @@ const Header = () => {
                             <LinkedIn sx={{ color: darkMode ? 'white' : '#171717' }} />
                         </IconButton>
 
-                        <Box sx={{ backgroundColor: 'black' }}>
+                        {/* Theme Mode Button */}
+                        <Box sx={{ backgroundColor: darkMode ? 'white' : '#171717' }}>
                             <Box
                                 component={motion.div}
                                 animate={animation ? { rotateY: 360 } : {}}
@@ -61,8 +115,8 @@ const Header = () => {
                             </IconButton>
                             </Box>
                         </Box>
-                    </Box>
 
+                    </Box>
                 </Toolbar>
                 </Container>
             </AppBar>
